159500da6dd48110389ef6b73202f51d079e499b
Witold Urbas <aiur18@student.bth.se>
2020-03-21 07:17:28 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+//Importing modules
+const express = require('express');
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require('mongoose');
+//Importing routes
+const authRoute = require("./routes/auth");
+
+
+//Config
+dotenv.config();
+app.use(express.urlencoded({ extended: false }));
+
+//connecting to db
+mongoose.connect( process.env.DB_CONNECT, {useNewUrlParser: true, useUnifiedTopology: true}, () =>
+    console.log("connected to DB")
+);
+
+//Index GET
+app.get("/", (req, res) => {
+    res.send("API running");
+});
+
+//middleware
+app.use(express.json());
+//Route middleware
+app.use("/api/user", authRoute);
+
+
+app.listen(3000, () => console.log("the server is running on posrt 3000"));
